c465cdb4700f189783e7bd0316d3bb03c3a00e2a
m-salikhov <andvary@inbox.ru>
2022-12-04 11:29:31 +0300

    c5

--- src/Helpers/_axios.ts
+++ src/Helpers/_axios.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 //Подключает куки и сервер
 export const _axios = axios.create({
   withCredentials: true,
-  baseURL: "https://62.217.179.200:5000",
+  baseURL: "https://andvarif.ru",
 });
