7e58ee6dc695a703bfb9b9e637e2d925766ad2bd
m-salikhov <andvary@inbox.ru>
2023-10-30 22:10:44 +0300

    style 30102023

--- src/Components/Elements/Question/Question.tsx
+++ src/Components/Elements/Question/Question.tsx
@@ -4,6 +4,7 @@ import Add from "./Add";
 import Answer from "./Answer";
 import { AiOutlineDown } from "react-icons/ai";
 import "./question.scss";
+import { Link } from "react-router-dom";
 
 interface Props {
   q: QuestionType;
@@ -16,7 +17,11 @@ const Question = ({ q, random }: Props) => {
     <div className="question">
       <div className="question__header">
         <h3>Вопрос {q.qNumber}</h3>
-        {random && <h4>из {q.tournament?.title}</h4>}
+        {random && (
+          <Link to={`tournament/${q.tournament?.id}`}>
+            {q.tournament?.title}
+          </Link>
+        )}
         {!random && <h3>Тур {q.tourNumber}</h3>}
       </div>
       {q.add && <Add add={q.add} />}
