c7342fd676def4cce0797b13af7e513a61e6ff6d
m-salikhov <andvary@inbox.ru>
2025-01-16 15:45:24 +0300

    tests: getDate, checkForm; store app hooks

---
+++ __tests__/UnitTests/getDate.test.ts
@@ -0,0 +1,20 @@
+import { getDate, getDateYYYY_MM_DD } from '../../src/Common/Helpers/getDate';
+import { expect, test, describe } from 'vitest';
+
+describe('getDateYYYY_MM_DD', () => {
+  test('should return correct date', () => {
+    const date = new Date('2022-06-01');
+    expect(getDateYYYY_MM_DD(date.getTime())).toBe('2022-06-01');
+  });
+});
+
+describe('getDate', () => {
+  test('should return correct date', () => {
+    const date = new Date('2022-06-01');
+    const dateStr = '1654041600000';
+    const dateNum = 1654041600000;
+    expect(getDate(dateStr)).toBe('01.06.2022');
+    expect(getDate(dateNum)).toBe('01.06.2022');
+    expect(getDate(date)).toBe('01.06.2022');
+  });
+});
