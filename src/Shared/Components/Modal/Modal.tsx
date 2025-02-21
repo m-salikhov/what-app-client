371ef52fb277f1f526e76c041674f93927e035cf
m-salikhov <andvary@inbox.ru>
2025-02-21 11:36:00 +0300

    modal: логика показа\скрытия скролла - реф-г

--- src/Shared/Components/Modal/Modal.tsx
+++ src/Shared/Components/Modal/Modal.tsx
@@ -1,18 +1,15 @@
-import { PropsWithChildren, useRef } from 'react';
 import './Modal.css';
+import { PropsWithChildren, useEffect } from 'react';
 import { animated, useTransition } from '@react-spring/web';
 import { scrollVisibility } from './Helpers/scrollVisibility';
 
 interface Props {
   active: boolean;
-  onClose: () => void;
+  onClose?: () => void;
   onDestroyed?: () => void;
 }
 //HOC для модальных окон.
 export default function Modal({ active, onClose, onDestroyed, children }: PropsWithChildren<Props>) {
-  //чтобы onDestroyed не срабатывал при первом рендере
-  const ref = useRef(null);
-
   const transition = useTransition(active, {
     from: {
       scale: 0.8,
@@ -28,29 +25,30 @@ export default function Modal({ active, onClose, onDestroyed, children }: PropsW
     },
 
     onDestroyed() {
-      if (!ref.current) {
+      if (!active) {
         scrollVisibility('show');
       }
 
-      if (onDestroyed && !ref.current) {
+      if (onDestroyed && !active) {
         onDestroyed();
       }
-    },
-
-    onStart() {
-      scrollVisibility('hide');
     },
 
     config: { duration: 200 },
   });
 
+  useEffect(() => {
+    if (active) {
+      scrollVisibility('hide');
+    }
+  }, [active]);
+
   return transition((style, active) =>
     active ? (
       <div
         className='modal'
-        ref={ref}
         onClick={(e) => {
-          if (e.target === e.currentTarget) {
+          if (e.target === e.currentTarget && onClose) {
             onClose();
           }
         }}
