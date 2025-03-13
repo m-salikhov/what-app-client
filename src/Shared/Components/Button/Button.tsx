6496ed5ee2ba319c6839fd21e75ccce4d93ba6bd
m-salikhov <andvary@inbox.ru>
2025-03-13 23:06:24 +0300

    Button: поправил пропсы

--- src/Shared/Components/Button/Button.tsx
+++ src/Shared/Components/Button/Button.tsx
@@ -1,27 +1,17 @@
 import './btn.css';
-import { FormEvent, MouseEvent } from 'react';
 
 interface ButtonProps {
-  title: string;
-  disabled?: boolean;
   extraClass?: string;
-  type?: 'button' | 'submit' | 'reset' | undefined;
-  id?: string;
-  onClick?: (e: MouseEvent) => void;
-  onSubmit?: (e: FormEvent<EventTarget>) => void;
 }
 
-export function Button({ onClick, title, extraClass, disabled, onSubmit, type, id }: ButtonProps) {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  extraClass?: string;
+}
+
+export function Button({ title, extraClass, type, ...props }: ButtonProps) {
   return (
     <div className='btn-elem'>
-      <button
-        className={extraClass ? `btn ${extraClass}` : 'btn'}
-        type={type || 'button'}
-        onClick={onClick}
-        onSubmit={onSubmit}
-        disabled={disabled}
-        id={id}
-      >
+      <button className={extraClass ? `btn ${extraClass}` : 'btn'} type={type || 'button'} {...props}>
         {title}
       </button>
     </div>
