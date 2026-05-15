de9af919cc636809cbbae59beeae51bbfa603a76
m-salikhov <andvary@inbox.ru>
2026-05-15 12:21:33 +0300

    config(ts): - baseUrl

--- src/Pages/Admin/Components/ModerateTournament/ModerateTournament.tsx
+++ src/Pages/Admin/Components/ModerateTournament/ModerateTournament.tsx
@@ -1,17 +1,34 @@
 import { skipToken } from "@reduxjs/toolkit/query";
 import { useParams } from "react-router-dom";
+import { Spinner } from "Shared/Components/Spinner/Spinner";
 import { useGetTournamentQuery } from "Store/ToolkitAPIs/tournamentAPI";
 
 export default function ModerateTournament() {
 	const { id } = useParams();
 	const {
 		data: tournament,
-		// isLoading,
-		// isSuccess,
-		// isError,
+		isLoading,
+		isSuccess,
+		isError,
 	} = useGetTournamentQuery(id ?? skipToken);
+
+	if (isError) return <h2>Ошибка при получении турнира</h2>;
+	if (isLoading) return <Spinner />;
+	if (!isSuccess) return null;
 
 	console.log(tournament);
 
-	return <p>Модерация турниров {id}</p>;
+	return (
+		<div>
+			<label>
+				<p> Название турнира</p>
+				<input
+					placeholder="Название турнира"
+					type="text"
+					// onChange={(e) => dispatch({ type: actionTypes.title, payload: e.target.value })}
+					value={tournament.title}
+				/>
+			</label>
+		</div>
+	);
 }
